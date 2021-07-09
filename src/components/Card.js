6709998a58c0f6fdada773db4329f8b9828a49ea
ewrt <ewrt101@gmail.com>
2021-07-09 12:53:25 +1200

    modal routing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
+
 function Card(props) {
+    const [ modalIsOpen, setModalIsOpen] = useState(false);
+
+
+    function runHandler() {
+        console.log("Clicked");
+        setModalIsOpen(true);
+    }
+
+    function closeModalHandler() {
+        console.log("Clicked");
+        setModalIsOpen(false);
+    }
+
+
     return (
         <div className='card'>
             <h2>{props.title}</h2>
             <div className='actions'>
-                <button className='btn'>Boop</button>
+                <button className='btn' onClick={runHandler}>Run</button>
             </div>
+
+             { modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/>}
+             { modalIsOpen && <Backdrop onCancel={closeModalHandler}/>}
         </div>
     );
 }
